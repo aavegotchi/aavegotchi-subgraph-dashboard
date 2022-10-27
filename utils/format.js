c0cefafdd4065770984770c49ab545e889e75e4b
Frank <frank.pfeift@protonmail.com>
2022-10-27 14:43:35 +0200

    added format number

--- utils/format.js
+++ utils/format.js
@@ -1,3 +1,11 @@
-export function formatHash(hash) {
-  return hash.substr(0, 4) + "..." + hash.substr(-4, 4);
+export function formatHash(hash, length = 8) {
+  return hash.substr(0, length) + "..." + hash.substr(-length, length);
+}
+
+export function formatNumber(number, minFractionDigits = 2) {
+  return number.toLocaleString(
+    undefined, // leave undefined to use the visitor's browser
+    // locale or a string like 'en-US' to override it.
+    { minimumFractionDigits: minFractionDigits }
+  );
 }
